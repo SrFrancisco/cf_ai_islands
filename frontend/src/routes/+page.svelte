7b3e0b342826509e23da5cb3142802287af0ea76
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2025-10-26 03:46:24 +0000

    fix: flexbox in start page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -18,18 +18,29 @@
   {#if data.islands?.length == 0}
     <span>(🏝️ No island found)</span>
   {/if}
-
+<div style="display: flex; flex-direction: row; gap: 15px; justify-content: space-evenly;">
   {#each data.islands as island}
 
-  <div style="display: inline-block; background-color: azure;
+  <!--<div style="display: inline-block; background-color: azure;
   border: 2px solid #56c8ff;
   border-radius: 5px; margin: 5px;">
     
-    <a href="/islands/{island}" style="text-decoration: none; color: black; padding: 5px 10px; margin: 5px; ">
-      <span style="font-size: 2em;">🏝️</span> {island}</a>
+    <a href="/islands/{island}" style="text-decoration: none; color: black; padding: 5px 10px; margin: 5px; font-size: 1.5em;">
+      <span>🏝️</span> {island}</a>
+  </div>-->
+
+
+<a href="/islands/{island}" class="card" style="border: 2px solid #0d6efd; text-decoration: none; display: flex; flex-direction: row; box-shadow: 5px 5px #69a5ff;">
+  <div class="card-img-top" style="background-color: #c1eeff; border-right: 2px solid  #0d6efd; font-size: 2.5em; flex: 1; border-radius: 5px 0 0 5px;"><span>🏝️</span></div>
+  <div class="card-body" style="flex: 2">
+    <span class="card-title" style="font-size: 1.3em;">{island}</span>
   </div>
+</a>
+
+
 
   {/each}
+</div>
 </div>
 
 
